31364c595a70469439f00e1581d845a1a9606b36
Nikola Zarkovic <nikolica.nidzica@gmail.com>
2018-06-11 10:58:29 +0200

    Added geolocation support via geolocation API

--- public/js/index.js
+++ public/js/index.js
@@ -14,17 +14,17 @@ socket.on('newMessage',function(message){
   console.log('New message', message);
   var li = jQuery('<li></li>');
   li.text(`${message.from}: ${message.text}`);
-
   jQuery('#messages').append(li);
-  //document.getElementById("demo").innerHTML = email.text;
 });
 
-// socket.emit('createMessage', {
-//   from: 'Frank',
-//   text: 'Hi'
-// }, function(data){
-//   console.log('Got it!',data);
-// });
+socket.on('newLocationMessage', function (message){
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current Location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+});
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
@@ -33,5 +33,22 @@ jQuery('#message-form').on('submit', function(e) {
     text: jQuery('[name=message]').val()
   }, function(/*data*/){
     // console.log('Got it!',data);
+  });
+});
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function(e){
+  if(!navigator.geolocation){
+    return alert('Geolocation not available in this browser!');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage',{
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  },function(){
+    alert('Unable to fetch location.');
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -8,6 +8,20 @@ describe('generateMessage()' ,() => {
     var message = generateMessage(from, text);
     expect(message).toInclude({from,text});
     expect(message.createdAt).toBeA('number');
+  });
+});
 
+var {generateLocationMessage} = require('./message')
+describe('generateLocationMessage', ()=>{
+  it('should generate correct location object', () => {
+    var from = 'Nikola';
+    var latitude = 45;
+    var longitude = 35;
+    var message = generateLocationMessage(from, latitude, longitude);
+    expect(message).toInclude({
+      from,
+      url:`https://www.google.com/maps?q=${latitude},${longitude}`
+    });
+    expect(message.createdAt).toBeA('number');
   });
 });
